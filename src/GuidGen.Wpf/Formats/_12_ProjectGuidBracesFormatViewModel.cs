39249ff865a623b0fc99d3b15d75f98bedcf32ca
Michael W Powell <mwpowellhtx@gmail.com>
2018-07-31 09:59:31 -0400

    added another format to the mix

---
+++ src/GuidGen.Wpf/Formats/_12_ProjectGuidBracesFormatViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace GuidGen.Formats
+{
+    // ReSharper disable once UnusedMember.Global
+    [DisplayOrder(12)]
+    public class ProjectGuidBracesFormatViewModel : FormatViewModel
+    {
+        private const string XmlTag = "ProjectGuid";
+
+        private static string GetXmlWithContent(string tag, string content)
+            => $"<{tag}>{content}</{tag}>";
+
+        private static string Formatter(Guid value, TextCase textCase)
+            => GetXmlWithContent(XmlTag, $@"{{{$"{value:D}".ToTextCase(textCase)}}}");
+
+        // ReSharper disable once UnusedMember.Global
+        public ProjectGuidBracesFormatViewModel(IGeneratorOptions options)
+            : base(options, GetXmlWithContent(XmlTag, $"{{xxxxxxxx-xxxx ... xxxx}}"), Formatter)
+        {
+        }
+    }
+}
